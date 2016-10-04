0588854cf26cefdcf26b573e463889ff80edcc90
Aniela M Wolkonowski <anielamw@gmail.com>
2016-10-04 15:37:53 -0400

    added first attemp at insert logic, failed to pass

--- import.rb
+++ import.rb
@@ -3,8 +3,6 @@
 require "pg"
 require "csv"
 
-psql building-database < schema.sql
-
 def db_connection
   begin
     connection = PG.connect(dbname: "building-database")
@@ -12,4 +10,28 @@ def db_connection
   ensure
     connection.close
   end
+end
+
+data = CSV.foreach('data.csv')
+
+db_connection do |conn|
+  data.each do |record|
+    conn.exec_params("INSERT INTO accounts (name, zoning_type_id, construction_type_id) VALUES ($1, $2, $3);", [record[0], zoning(record[1].to_i), constructing(record[2].to_i)])
+  end
+end
+
+def zoning(zoning_type)
+  db_zoning = conn.exec("SELECT * FROM zoning_types;")
+  unless db_zoning.include?(zoning_type)
+    conn.exec("INSERT INTO zoning_types (name) VALUES ($1);", [zoning_type])
+  end
+  conn.exec("SELECT id FROM zoning_types WHERE zoning_types.name = '#{zoning_type}';")
+end
+
+def constructing(construction_type)
+  db_construction = conn.exec("SELECT * FROM construction_types")
+  unless db_construction.include?(construction_type)
+    conn.exec("INSERT INTO construction_types (name) VALUES ($1);", [construction_type])
+  end
+  conn.exec("SELECT id FROM construction_types WHERE construction_types.name = '#{construction_type}';")
 end
