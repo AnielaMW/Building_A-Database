d417200be0e3295237e84c368d35c91069fa2e22
Aniela M Wolkonowski <anielamw@gmail.com>
2016-10-04 23:30:29 -0400

    added third attemp at insert logic, passed

--- import.rb
+++ import.rb
@@ -17,9 +17,10 @@ end
 
 def zoning(zoning_type)
   db_connection do |conn|
-    db_zoning = conn.exec("SELECT name FROM zoning_types;")
-    unless db_zoning.include?(zoning_type)
-      conn.exec("INSERT INTO zoning_types (name) VALUES ($1);", [zoning_type])
+    db_zoning = conn.exec("SELECT name FROM zoning_types;").to_a
+    zoning_array = db_zoning.map { |zone_hash| zone_hash["name"] }
+    unless zoning_array.include?(zoning_type)
+      conn.exec_params("INSERT INTO zoning_types (name) VALUES ($1);", [zoning_type])
     end
     conn.exec("SELECT id FROM zoning_types WHERE zoning_types.name = '#{zoning_type}';").to_a[0]["id"].to_i
   end
@@ -27,9 +28,14 @@ end
 
 def constructing(construction_type)
   db_connection do |conn|
-    db_construction = conn.exec("SELECT name FROM construction_types")
-    unless db_construction.include?(construction_type)
-      conn.exec("INSERT INTO construction_types (name) VALUES ($1);", [construction_type])
+    # I wanted this to work, but it did not and I don't know why. Maybe ask an EE later this week.
+      # unless conn.exec_params("SELECT name FROM construction_types WHERE name = $1", [construction_type])
+      #   conn.exec_params("INSERT INTO construction_types (name) VALUES ($1);", [construction_type])
+      # end
+    db_construct = conn.exec("SELECT name FROM construction_types;").to_a
+    construct_array = db_construct.map { |construct_hash| construct_hash["name"] }
+    unless construct_array.include?(construction_type)
+      conn.exec_params("INSERT INTO construction_types (name) VALUES ($1);", [construction_type])
     end
     conn.exec("SELECT id FROM construction_types WHERE construction_types.name = '#{construction_type}';").to_a[0]["id"].to_i
   end
